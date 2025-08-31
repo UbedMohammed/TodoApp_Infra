3a60a7821afc8854ad5b5840922d6afac3025c29
Ubed Mohammed <ubed.mohammed@example.com>
2025-08-31 14:14:54 +0100

    infracost added

---
+++ Environment/Dev/variables.tf
@@ -0,0 +1,8 @@
+# variable "container_registry" {}
+# variable "kv" {}
+# variable "aks" {}
+# variable "mid" {}
+variable "rg" {}
+# variable "sql_database" {}
+# variable "sql_server" {}
+variable "stg" {}
